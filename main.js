efb92e31712416453e1aa1876660a4a66b39b191
johnnyaokay <49286386+johnnyaokay@users.noreply.github.com>
2021-10-02 20:03:27 +0100

    tle position calculation

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as satellite from 'satellite.js';
 
 const scene = new THREE.Scene()
 const camera = new THREE.
@@ -31,13 +32,14 @@ const sphere = new THREE.Mesh(
 scene.add(sphere)
 camera.position.z = 3
 
-function calcPosFromLatLonRad(lat, lon) {
+function calcPosFromLatLonRad(lat, lon, height) {
     var phi = (90 - lat) * (Math.PI / 180)
     var theta = (lon + 180) * (Math.PI / 180)
+    var heigth = (lon + 180) * (Math.PI / 180)
 
     let x = -(Math.sin(phi) * Math.cos(theta))
     let z = (Math.sin(phi) * Math.sin(theta))
-    let y = (Math.cos(phi + 50))
+    let y = (Math.cos(phi))
 
     return { x, y, z }
 }
@@ -48,18 +50,18 @@ let mesh = new THREE.Mesh(
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
 
-let point1 = {
-    lat: 50.4501,
-    lng: 30.5234
-}
+// let point1 = {
+//     lat: 50.4501,
+//     lng: 30.5234
+// }
 
-let mexico = {
-    lat: 32.1656,
-    lng: -82.9001
-}
+// let mexico = {
+//     lat: 32.1656,
+//     lng: -82.9001
+// }
 
-//let pos = convertLatLngToCartesian(point1)
-let pos = calcPosFromLatLonRad(mexico.lat, mexico.lng)
+// //let pos = convertLatLngToCartesian(point1)
+// let pos = calcPosFromLatLonRad(mexico.lat, mexico.lng)
 
 const loader = new THREE.FileLoader();
 
@@ -76,8 +78,40 @@ loader.load(
         var line = data.split('\n')
         var count = 0
         line.forEach(line => {
+            if (line[0] == "1") {
+                tle[count] = {}
+                tle[count][0] = line
+            } else if (line[0] == "2") {
+                tle[count][1] = line
 
+
+                const satrec = satellite.twoline2satrec(
+                    tle[count][0], tle[count][1]
+                )
+
+                const date = new Date()
+                const positionAndVelocity = satellite.propagate(satrec, date)
+                const gmst = satellite.gstime(date)
+                const tlePos = satellite.eciToGeodetic(positionAndVelocity.position, gmst)
+
+                let mesh = new THREE.Mesh(
+                    new THREE.SphereBufferGeometry(0.01, 10, 10),
+                    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+                )
+
+                console.log(tlePos.longitude)
+
+                let pos = calcPosFromLatLonRad(tlePos.latitude, tlePos.longitude, tlePos.height)
+
+                mesh.position.set(pos.x, pos.y, pos.z)
+
+                scene.add(mesh)
+
+                count++
+            }
         })
+
+
     },
 
     // onProgress callback
@@ -91,12 +125,14 @@ loader.load(
     }
 );
 
-console.log(pos.x)
-mesh.position.set(pos.x, pos.y, pos.z)
+
+
+
+// mesh.position.set(pos.x, pos.y, pos.z)
 
 //mesh.position.set(1, 0, 0)
 
-scene.add(mesh)
+// scene.add(mesh)
 
 function animate() {
     controls.update()
