2ea8f15a488a961a52d2a8b095d1bc15bc395a2f
eronaaliu <50829986+eronaaliu@users.noreply.github.com>
2021-10-02 19:57:19 +0100

    added atmospheric vibes in fragment shader

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,7 +1,11 @@
- varying vec2 vertexUV; // UV passed into fragment shader
+varying vec2 vertexUV; // UV passed into fragment shader
+varying vec3 vertexNormal;
 
- void main() {
-     //set it as vertex shader runs
-     vertexUV = uv;
-     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
- }
+void main() {
+    //set it as vertex shader runs
+    //https://threejs.org/docs/#api/en/renderers/webgl/WebGLProgram
+    vertexUV = uv;
+    vertexNormal = normal;
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
