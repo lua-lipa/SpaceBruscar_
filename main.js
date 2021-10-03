76edefa996eeb216a9c6817bd545b8e354df7141
johnnyaokay <49286386+johnnyaokay@users.noreply.github.com>
2021-10-03 12:38:55 +0100

    added window resize

--- main.js
+++ main.js
@@ -27,6 +27,25 @@ document.body.appendChild(renderer.domElement)
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true
 
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
 const radius = 10
 const radiusKm = 6371
 
